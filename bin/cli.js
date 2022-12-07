fae964f61920fcfb9652f768de202f998c0dc0b1
bilen2020 <64380790+bilen2020@users.noreply.github.com>
2022-12-07 18:40:35 -0500

    Added files

---
+++ bin/cli.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+import minimist from "minimist";
+import {roll} from '../lib/roll.js'
+
+const args = minimist(process.argv.slice(2));
+
+var sides = 6;
+var dice = 2;
+var rolls = 1;
+
+if(args.sides) {
+    sides = args.sides;
+}
+if(args.dice) {
+    dice = args.dice;
+}
+if(args.rolls) {
+    rolls = args.rolls;
+}
+
+let result = roll(sides, dice, rolls);
+console.log(JSON.stringify(result));
